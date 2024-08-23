72b3b6942f6afe95f8b4123edc95d3227f9161f9
Sankalpa-01 <161730339+Sankalpa-01@users.noreply.github.com>
2024-08-23 11:33:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //Treat all JS Code as newer version
+
+//Use tc39.es and MDN to take the javascipt reference
+//alert(3 + 3)  //We are using Node JS not Browser.
+
+//Maintain a high Code Read-ability.
+console.log(3 + 3);
+console.log("Sankalpa");
+
+let name = "Navyansh"
+let age  = 18
+let isLoggedIn = false;
+
+//                                            PRIMITIVE DATA-TYPES
+//Number => 2 to the power of 53
+//bigint => It is used to store if the number is very large.
+//String => ("")It is used to store name,letters, etc. 
+//Boolean => TRUE/FALSE
+//null => It is a standalone value(empty value)
+//undefined => It is not defined yet and will be defined further/in future.
+//symbol => Use to find the uniqueness.
+
+//object
+
+console.log(typeof age);
+
+console.log(typeof null);//Object
+console.log(typeof undefined);//undefined
+
