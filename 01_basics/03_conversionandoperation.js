0c858db63d2fa0f90c76fb325a4b555b439b979f
Sankalpa-01 <161730339+Sankalpa-01@users.noreply.github.com>
2024-08-24 16:53:15 +0000

    conversion of data types

---
+++ 01_basics/03_conversionandoperation.js
@@ -0,0 +1,18 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//NaN means (Not an Number).
+
+//"33" => 33
+//"33abs" => NaN
+//true => 1
+//false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
