c94fe51d5279531927a7de7019da18d6b3391d1d
Sankalpa-01 <161730339+Sankalpa-01@users.noreply.github.com>
2024-08-25 05:16:53 +0000

    operations

--- 01_basics/03_conversionandoperation.js
+++ 01_basics/03_conversionandoperation.js
@@ -1,3 +1,5 @@
+//************CONVERSIONS***********
+
 let score = null
 
 console.log(typeof score);
@@ -16,3 +18,35 @@ let isLoggedIn = 1
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
 console.log(booleanisLoggedIn);
+
+
+//***************OPERATIONS****************
+ 
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+/* => MEANS RAISE TO THE POWER */
+
+let str1 = "hello"
+let str2 = " Sankalpa"
+
+let str3 = str1 + str2
+console.log(str3);
+
+//Joh pehela aayega (like string or integer) wahi baki saare part ka bhi same ho jayega.
+console.log("1"+ 2 + 2);
+console.log(1 + 2 + "3");
+
+//Don't use this type of operation done below
+console.log(+true);//yeh one dega kyun ki increment operator use hua toh pehele boolean integer me convert hoga phir increment/decrement use hoga.
+
+
+
+
+
+
+
+
+
+
