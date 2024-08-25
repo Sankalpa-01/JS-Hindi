a311b846f103d068a496793a3cfc335721a755a8
Sankalpa-01 <161730339+Sankalpa-01@users.noreply.github.com>
2024-08-25 05:53:59 +0000

    datatypes summary

---
+++ 01_basics/05-datatypes_summary.js
@@ -0,0 +1,35 @@
+//Primitive Data-Types
+/*There are 7 primitive data-types:
+String,Number,Boolean,null,undefined,Symbol,BigInt*/
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const Outsidetemp = null
+let userEmail;//It will be undefined
+
+const  Id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(Id === anotherId);
+
+const bigNumber = 345667889876n
+
+//Refernce Types(Non-Primitive)
+/*Arrays,Objects,Functions*/
+
+//Arrays
+const heroes = ["shaktimaan", "naagraj", "doga"];
+
+//Objects
+let myObj = {
+    name: "Sankalpa",
+    age: 21,
+}
+
+//Functions
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
