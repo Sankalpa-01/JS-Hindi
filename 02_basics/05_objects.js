1e70f2247f5eae5e0b02f45ac0913b0b4eefacdf
Sankalpa-01 <161730339+Sankalpa-01@users.noreply.github.com>
2024-08-27 10:15:33 +0000

    objects and jason talks

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,19 @@
+//OBJECTS AND JSON API INTRODUCTION
+
+//DESTRUCTURING
+const course = {
+    coursename : "JS IN HINDI",
+    price : "99",
+    courseInstructor: "hitesh"
+}
+
+const {courseInstructor : instructor} = course//Example of Destructuring
+console.log(instructor);
+
+//                                                  API BASIC THINGS
+// {
+//     "name" : "SANKALPA",
+//     "coursename" : "JS-IN-HINDI",
+//     "price" : "free"
+// }
+
