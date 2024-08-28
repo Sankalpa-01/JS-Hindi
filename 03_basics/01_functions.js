d90436de6ae550c42095b43db2983a572783ad69
Sankalpa-01 <161730339+Sankalpa-01@users.noreply.github.com>
2024-08-28 09:45:40 +0000

    done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -47,3 +47,35 @@ function loginUserMessage(username ){
 //without any things inside the bracket it will give undefined.
 console.log(loginUserMessage("sankalpa"));
 console.log(loginUserMessage());
+
+// ... => This operator is called a rest operator as well as spraed operator
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyObject)
+{
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);   
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
